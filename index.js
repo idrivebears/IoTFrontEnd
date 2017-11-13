6334c348afc8ba5d99fd932d4e7f14920ec07124
Alex Walls <idrivebears@gmail.com>
2017-11-12 19:28:02 -0600

    added room picker

--- index.js
+++ index.js
@@ -62,22 +62,6 @@ router.use(function (req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
-router.route('/room/:room_id')
-    .get(function (req, res) {
-        var roomId = req.params.room_id;
-
-        console.log('Request made to room');
-        console.log(roomId);
-        var data;
-        var info = getRoomInfo(roomId, 1, function (error, response, body) {
-            data = JSON.parse(body);
-            console.log(data);
-            var roomName = 'Salón ' + roomId;
-            var message = roomName + ': ' + 'Temperatura Actual';
-            res.render('room_view', { title: roomName, message: message, temperature: '24.0' });
-        });
-    });
-
 router.route('/room/stats/:room_id/')
     .get(function (req, res) {
         var roomId = req.params.room_id;
@@ -91,7 +75,24 @@ router.route('/room/stats/:room_id/')
             var roomName = 'Salón ' + roomId;
             var message = roomName + ': ' + 'Historial de Temperatura';
             var roomHistory = getRoomHistory(data.feeds);
-            res.render('room_history_view', { title: roomName, message: message, roomHistory: roomHistory });
+            res.render('room_stats_view', { title: roomName, message: message, roomHistory: roomHistory });
+        });
+    });
+
+router.route('/room_pick/')
+    .get(function (req, res) {
+        var roomId = req.params.room_id;
+
+        console.log('Request made to room');
+        console.log(roomId);
+        var data;
+        var info = getRoomInfo(roomId, 20, function (error, response, body) {
+            data = JSON.parse(body);
+            //console.log(data.feeds);
+            var roomName = 'Salón ' + roomId;
+            var message = roomName + ': ' + 'Historial de Temperatura';
+            var roomHistory = getRoomHistory(data.feeds);
+            res.render('room_picker', { title: roomName, message: message, roomHistory: roomHistory });
         });
     });
 
